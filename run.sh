fba6649ca3589e981381daaed46f85c5231a169f
jonlt <github@j20n.com>
2022-05-14 01:14:36 +0200

    fix run script

--- run.sh
+++ run.sh
@@ -2,5 +2,4 @@
 SCRIPT=$(realpath $0)
 SCRIPTPATH=$(dirname $SCRIPT)
 
-docker run --rm -v $SCRIPTPATH:/app/db --env-file $SCRIPTPATH/.env kolding-horing-scraper
-
+docker run --rm -v ${SCRIPTPATH}:/app/db --env-file ${SCRIPTPATH}/.env kolding-horing-scraper
